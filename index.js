4d1256b742cb4efcd724f8a56891ce8cf515ca07
JacksonJ97 <jacksonjude97@gmail.com>
2021-07-29 21:09:39 -0400

    finished mobile view

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const articleFooter = document.querySelector(".article-footer");
 const shareBtn = document.querySelector(".share-btn");
 const shareContainer = document.querySelector(".share-container");
-const sendShareBtn = document.querySelector(".send-share-btn");
+const darkShareBtn = document.querySelector(".dark-share-btn");
 
 articleFooter.style.display = "flex";
 shareContainer.style.display = "none";
-console.log(shareContainer.style.display);
-console.log(articleFooter.style.display);
+
+function toggleDisplay() {
+  if (shareContainer.style.display === "none" && articleFooter.style.display === "flex") {
+    articleFooter.style.display = "none";
+    shareContainer.style.display = "block";
+  } else {
+    articleFooter.style.display = "flex";
+    shareContainer.style.display = "none";
+  }
+}
 
 shareBtn.addEventListener("click", function () {
-  if (shareContainer.style.display === "none" && articleFooter.style.display === "flex") {
-    articleFooter.style.display = "none";
-    shareContainer.style.display = "block";
-  } else {
-    articleFooter.style.display = "flex";
-    shareContainer.style.display = "none";
-  }
+  toggleDisplay();
 });
 
-sendShareBtn.addEventListener("click", function () {
-  if (shareContainer.style.display === "none" && articleFooter.style.display === "flex") {
-    articleFooter.style.display = "none";
-    shareContainer.style.display = "block";
-  } else {
-    articleFooter.style.display = "flex";
-    shareContainer.style.display = "none";
-  }
+darkShareBtn.addEventListener("click", function () {
+  toggleDisplay();
 });
